3766ab3ca6f814ae66d2502949199de67d508f9b
AB-Siddique278 <absiddique1060@gmail.com>
2022-03-01 01:45:53 +0600

    add phone details

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const displaySearchResult = data =>{
             <h5 class="card-title">Name: ${phoneData.phone_name}</h5>
             <h6>Brand: ${phoneData.brand}</h6>
             <div>
-            <button type="button" class="btn btn-primary">Show Details</button>
+            <button onclick="loadPhoneName('${phoneData.phone_name}')" type="button" class="btn btn-primary">Show Details</button>
             
             </div>
 
@@ -48,3 +48,40 @@ const displaySearchResult = data =>{
 
     })
 }
+
+const loadPhoneName = iphone=>{
+
+    const url =`https://openapi.programming-hero.com/api/phones?search=${iphone}`;
+    console.log(url);
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayphoneDetail(data.data[0]));
+
+
+}
+
+const displayphoneDetail = phone =>{
+    const countryDiv = document.getElementById('phone-detail');
+    countryDiv.innerHTML=`
+
+    <h2></h2>
+    <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+        <div class="col-md-4">
+        <img src="${phone.image}" class="img-fluid rounded-start" alt="...">
+        </div>
+            <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text"> ${phone.brand}</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+    </div>
+    </div>
+    </div>
+    
+    `
+    console.log(phone);
+}
+
